04f8a106bc16ae5433e7b4453db412585617ba3b
zxuu0105 <zxuu0105@student.monash.edu>
2024-09-28 21:22:40 +1000

    Update vega_lite_vis2.js

--- vega_lite_vis2.js
+++ vega_lite_vis2.js
@@ -1,10 +1,10 @@
 
 // 加载并嵌入地图可视化文件
-vegaEmbed('#vis_map', 'wk10map.vg.json').then(function(result) {
+vegaEmbed('#vis_map', 'visualization.vl (1).json').then(function(result) {
     // 处理地图结果或其他逻辑
   }).catch(console.error);
   
   // 加载并嵌入柱状图可视化文件
-  vegaEmbed('#vis_bar', 'wk10barchart.vg.json').then(function(result) {
+  vegaEmbed('#vis_bar', 'visualization.vl (2).json').then(function(result) {
     // 处理柱状图结果或其他逻辑
   }).catch(console.error);
